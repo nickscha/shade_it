856d0f7d0ee38c825fb986c6e14263db02f90060
nickscha <nick@scharnbeck.de>
2026-01-23 11:34:30 +0100

    [BUILD] specify entry point

--- win32_shade_it_build.bat
+++ win32_shade_it_build.bat
@@ -5,7 +5,7 @@ set PLATFORM_NAME=win32_shade_it
 set DIST_DIR=dist
 
 set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native ^
--std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe -Xlinker /STACK:0x100000,0x100000 ^
+-std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe -Xlinker /STACK:0x100000,0x100000 -Xlinker /ENTRY:mainCRTStartup ^
 -fno-builtin -ffreestanding -fno-asynchronous-unwind-tables -fuse-ld=lld -fno-math-errno -fno-trapping-math ^
 -Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wsign-conversion ^
 -Wmissing-field-initializers -Wuninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
