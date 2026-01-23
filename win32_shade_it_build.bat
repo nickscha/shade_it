ac4dcfd3de7c221c4e6e9e81a375b8e2a220e40e
nickscha <nick@scharnbeck.de>
2026-01-23 11:34:59 +0100

    [BUILD] remove unused DIST_DIR variable

--- win32_shade_it_build.bat
+++ win32_shade_it_build.bat
@@ -2,7 +2,6 @@
 REM Compiles the program without the C standard library
 
 set PLATFORM_NAME=win32_shade_it
-set DIST_DIR=dist
 
 set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native ^
 -std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe -Xlinker /STACK:0x100000,0x100000 -Xlinker /ENTRY:mainCRTStartup ^
