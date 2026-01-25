1bc3394df904349751db64d03f9df4d6bc376cde
nickscha <nick@scharnbeck.de>
2026-01-25 10:31:19 +0100

    [EXAMPLE] make waves shader more readable

--- examples/shade_it_waves.fs
+++ examples/shade_it_waves.fs
@@ -13,10 +13,13 @@ uniform float iFrameRate;
 void mainImage(out vec4 f, vec2 u) {
     float s=0,i,n;
     vec3 r = iResolution,p;
-    for(u = (u-r.xy/2.)/r.y-s; i++ < 32. && ++s>.001;)
-        for (p += vec3(u*s,s),s = p.y,
-            n =.01; n < 1.;n+=n)
+
+    for(u = (u-r.xy/2.)/r.y-s; i++ < 32. && ++s>.001;) {
+        for (p += vec3(u*s,s),s = p.y, n =.01; n < 1.;n+=n) {
             s += abs(dot(sin(p.z + iTime + p/n),  r/r)) * n*.1;
+        }
+    }
+
     f = tanh(i*vec4(5,2,sin(iTime),0)/length(u-.1)/5e2);
 }
 
